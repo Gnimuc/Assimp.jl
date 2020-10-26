2be3e9d664da14e99aef7426a670fc9e2aea8ec5
Gnimuc <qiyupei@gmail.com>
2020-10-26 20:28:10 +0800

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using Assimp
+using Assimp.LibAssimp
 using Test
 
-@testset "Assimp.jl" begin
-    # Write your own tests here.
+function import_file_test(filename)
+    scene = aiImportFile(joinpath(@__DIR__, filename), 
+                         aiProcess_CalcTangentSpace |
+                         aiProcess_Triangulate |
+                         aiProcess_JoinIdenticalVertices |
+                         aiProcess_SortByPType
+                        )
+    if scene == C_NULL
+        @error aiGetErrorString() |> unsafe_string
+        return false
+    end
+    aiReleaseImport(scene)
+    return true
 end
+
+@testset "Assimp.jl" begin
+    @test import_file_test("test.glb")
+    @test import_file_test("test.fbx")
+    @test import_file_test("test.stl")
+end
+
+
