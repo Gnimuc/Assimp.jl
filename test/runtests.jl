5fddde249853eac79969f6854c831f446862aaea
Gnimuc <qiyupei@gmail.com>
2021-05-10 22:39:58 +0900

    Add an example for testing the mesh loader

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,3 +22,27 @@ end
     @test import_file_test("test.fbx")
     @test import_file_test("test.stl")
 end
+
+@testset "Show vertices" begin
+    scene = aiImportFile(joinpath(@__DIR__, "test.stl"),
+                         aiProcess_CalcTangentSpace |
+                         aiProcess_Triangulate |
+                         aiProcess_JoinIdenticalVertices |
+                         aiProcess_SortByPType
+                         )
+    if scene == C_NULL
+        @error aiGetErrorString() |> unsafe_string
+        @test false
+    end
+
+    scene_obj = unsafe_load(scene)
+    num_mesh = scene_obj.mNumMeshes
+    for i = 1:num_mesh
+        mesh_obj = unsafe_load(unsafe_load(scene_obj.mMeshes, i))
+        for j = 1:mesh_obj.mNumVertices
+            vt = unsafe_load(mesh_obj.mVertices, j)
+            @show vt
+        end
+    end
+    aiReleaseImport(scene)
+end
